c668bf6466155a5ff861abd14b0beb66714d83e6
ouafaema <58174694+ouafaema@users.noreply.github.com>
2024-11-13 12:07:01 +0100

    index + Query N + 1

--- src/main/java/com/exo1/exo1/repository/ProjetRepository.java
+++ src/main/java/com/exo1/exo1/repository/ProjetRepository.java
@@ -1,8 +1,12 @@
 package com.exo1.exo1.repository;
 
 import com.exo1.exo1.entity.Projet;
-import com.exo1.exo1.entity.Utilisateur;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ProjetRepository extends JpaRepository<Projet, Long> {
+    @EntityGraph(attributePaths = {"taches"})
+    List<Projet> findAllWithTaches();
 }
